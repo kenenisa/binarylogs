513773e3cf4c05be52708c3c6bbafb9e0c511c48
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2021-08-18 17:05:45 +0300

    fixed bugs, added animation, changed other stuff too

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function reduceShare(interval) {
     totalShare += interval.total;
 }
 function calcShare(interval) {
-    interval.dayShare = Math.floor((interval.day * 100) / dayShare);
-    interval.weekShare = Math.floor((interval.week * 100) / weekShare);
-    interval.monthShare = Math.floor((interval.month * 100) / monthShare);
-    interval.totalShare = Math.floor((interval.total * 100) / totalShare);
+    interval.dayShare = (interval.day * 100) / dayShare;
+    interval.weekShare = (interval.week * 100) / weekShare;
+    interval.monthShare = (interval.month * 100) / monthShare;
+    interval.totalShare = (interval.total * 100) / totalShare;
     return interval
 }
 function calcInterval(logs) {
@@ -29,13 +29,14 @@ function calcInterval(logs) {
     interval.month = 0
     interval.total = 0
     logs.forEach(log => {
-        if (log.from > day) {
+        const fromNow = new Date().getTime() - log.from;
+        if (fromNow < day) {
             interval.day += log.duration
         }
-        if (log.from > week) {
+        if (fromNow < week) {
             interval.week += log.duration
         }
-        if (log.from > month) {
+        if (fromNow < month) {
             interval.month += log.duration
         }
         interval.total += log.duration
@@ -51,7 +52,7 @@ function calcDurations(incoming) {
     const length = logs.length;
     for (let i = 0; i < length / 2; i++) {
         const duration = logs[0].at - logs[1].at;
-        newLogs.push({ from: logs[0].at, duration });
+        newLogs.push({ from: logs[1].at, duration });
         logs.shift()
         logs.shift();
     }
@@ -62,13 +63,16 @@ function sortLogs(logs) {
     return calcDurations(logs)
 }
 let peopleStore = [];
+let visibleItem = '';
 function openPerson(idd) {
-    const query = peopleStore.find(pe => pe.id === idd);
-    if (query) {
-        const person = query;
-        const intr = person.interval;
-        id('person').className = 'right'
-        id('person').innerHTML = `
+    if (visibleItem !== idd) {
+        visibleItem = idd;
+        const query = peopleStore.find(pe => pe.id === idd);
+        if (query) {
+            const person = query;
+            const intr = person.interval;
+            id('person').className = 'right'
+            id('person').innerHTML = `
         <div class="top">
           <div class="avatar">
             <img src="https://robohash.org/${person.name}" alt="avatar" />
@@ -81,19 +85,19 @@ function openPerson(idd) {
         <div class="bottom">
           <div class="stat">
             <span class="time">Today</span>
-            <span class="value">${intr.dayShare}% . ${parseTime(intr.day)}</span>
+            <span class="value">${odp(intr.dayShare)}% · ${parseTime(intr.day)}</span>
           </div>
           <div class="stat">
             <span class="time">Last week</span>
-            <span class="value">${intr.weekShare}% . ${parseTime(intr.week)}</span>
+            <span class="value">${odp(intr.weekShare)}% · ${parseTime(intr.week)}</span>
           </div>
           <div class="stat">
             <span class="time">Last month</span>
-            <span class="value">${intr.monthShare}% . ${parseTime(intr.month)}</span>
+            <span class="value">${odp(intr.monthShare)}% · ${parseTime(intr.month)}</span>
           </div>
           <div class="stat">
             <span class="time">Total</span>
-            <span class="value">${intr.totalShare}% . ${parseTime(intr.total)}</span>
+            <span class="value">${odp(intr.totalShare)}% · ${parseTime(intr.total)}</span>
           </div>
         </div>
         <h3>Logs</h3>
@@ -102,8 +106,17 @@ function openPerson(idd) {
         </div>
       </div>
         `;
-        // id('')
+            // id('')
+        }
     }
+
+}
+function odp(num) {
+    const fl = Math.floor(num);
+
+    if (fl === num) return num;
+    const tbr = num.toString().split('.')[1].slice(0, 2);
+    return Math.floor(num) + '.' + Math.round(Number(tbr[0] + '.' + tbr[1]));
 }
 ref.get().then((querySnapshot) => {
     let people = [];
@@ -147,19 +160,19 @@ ref.get().then((querySnapshot) => {
                 peopleStore = people;
                 id('top-total').className = 'total';
                 id('top-total').innerHTML = `
-                    <span><i>total</i> ${Math.floor(totalShare / hour)}h</span> . 
-                    <span><i>today</i> ${Math.floor(dayShare / hour)}h</span> . 
-                    <span><i>last week</i> ${Math.floor(weekShare / hour)}h<h</span> . 
-                    <span><i>last month</i> ${Math.floor(monthShare / hour)}h<h</span>`
+                    <span><i>total</i> ${Math.floor(totalShare / hour)}h</span> · 
+                    <span><i>today</i> ${Math.floor(dayShare / hour)}h</span> ·
+                    <span><i>last week</i> ${Math.floor(weekShare / hour)}h</span> · 
+                    <span><i>last month</i> ${Math.floor(monthShare / hour)}h</span>`
             }
             people.sort((x, y) => {
                 return y.logData[0].from - x.logData[0].from;
             })
-            people.sort((x,y)=>{
-                if(x.online){
+            people.sort((x, y) => {
+                if (x.online) {
                     return -1;
                 }
-                if(y.online){
+                if (y.online) {
                     return 1;
                 }
                 return 0
@@ -172,7 +185,7 @@ ref.get().then((querySnapshot) => {
                 </div>
                 <div class="text">
                 <div class="name">${person.name}</div>
-                <div class="des">${Math.floor(person.interval.totalShare)}% . ${parseTime(person.interval.total)}</div>
+                <div class="des">${odp(person.interval.totalShare)}% · ${parseTime(person.interval.total)}</div>
                 </div>
                 ${person.online ? '<div class="indicator"></div>' : ''}
                 </div>`
